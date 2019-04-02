f9c180050e01fbc5632b2cec53b553e4079eb9fd
Daniel Young <daniel.young@TradeMe.co.nz>
2019-04-03 10:01:19 +1300

    Complete "Codewars 500" kata

--- codewars-500.js
+++ codewars-500.js
@@ -8,7 +8,24 @@ function solution() {
     return new Promise((resolve, reject) => {
         request(URL, (error, response, body) => {
             const $ = cheerio.load(body);
-            console.log($('.leaderboard').text());
+            const leaderboard = { position: {} }
+            $('.leaderboard tr')
+                .slice(1) // Skip header
+                .each((index, element) => {
+                    const $element = $(element);
+                    const name = $element.data('username');
+                    const rank = $element.children().first().text().slice(1);
+                    const clan = $element.children().eq(2).text();
+                    const honor = +$element.children().eq(3).text().replace(/,/g, '');
+
+                    leaderboard.position[rank] = {
+                        name,
+                        clan,
+                        honor
+                    };
+                });
+
+            resolve(leaderboard)
         });
-    });   
+    });
 }
